3b10f20910925a72d3dade06fa5182a3acfc2fed
B3Max <90093050+B3Max@users.noreply.github.com>
2021-12-07 21:19:18 +0300

    Add files via upload

---
+++ KR-OspeshinskiyMA/js/clock.js
@@ -0,0 +1,19 @@
+function showTime() {
+  var now = new Date();
+  var sessia = new Date(2061, 5, 28, 0, 0, 0, 0);
+  var difference = sessia - now;
+
+  var seconds = Math.floor((difference / 1000) % 60);
+  var minutes = Math.floor((difference / 1000 / 60) % 60);
+  var hours = Math.floor((difference / 1000 / 60 / 60) % 24);
+  var days = Math.floor((difference / 1000 / 60 / 60 / 24) % 365);
+  var years = Math.floor(difference / 1000 / 60 / 60 / 24 / 365);
+
+  document.querySelector(".years").innerHTML = years + "<br>" + "Лет";
+  document.querySelector(".days").innerHTML = days + "<br>" + "Дней";
+  document.querySelector(".hours").innerHTML = hours + "<br>" + "Часов";
+  document.querySelector(".minutes").innerHTML = minutes + "<br>" + "Минут";
+  document.querySelector(".seconds").innerHTML = seconds + "<br>" + "Секунд";
+
+  setTimeout("showTime()", 1000);
+}
